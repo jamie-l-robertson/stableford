963a0c83d6eae104fc0d0c7bf11ba9dc2338559d
Jamie Robertson <jamie@line.uk.com>
2018-07-22 20:37:46 +0100

    Added check for published profiles

--- src/components/players/index.js
+++ src/components/players/index.js
@@ -9,6 +9,7 @@ const PLAYERS_Q = gql`
     players {
       id
       name
+      status
     }
   }
 `;
@@ -19,17 +20,23 @@ class Players extends React.Component {
       <PlayersWrapper>
         <Query query={PLAYERS_Q}>
           {({ loading, error, data }) => {
+            console.log(data);
             return (
               <React.Fragment>
                 <ul>
                   {error ? <li>{error}</li> : null}
                   {loading ? <li>Loading...</li> : null}
                   {data.players &&
-                    data.players.map(player => (
-                      <li key={player.id}>
-                        <Link to={`/players/${player.id}`}>{player.name}</Link>
-                      </li>
-                    ))}
+                    data.players.map(
+                      player =>
+                        player.status === "PUBLISHED" ? (
+                          <li key={player.id}>
+                            <Link to={`/players/${player.id}`}>
+                              {player.name}
+                            </Link>
+                          </li>
+                        ) : null
+                    )}
                 </ul>
               </React.Fragment>
             );
